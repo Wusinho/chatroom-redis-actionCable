0eea3934de0337ad053ea983780f8d368a76cc17
heber <heberlazo@gmail.com>
2022-02-01 17:58:36 -0500

    Added rooms controller

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,17 @@
 class RoomsController < ApplicationController
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(rooms_params)
+  end
+
+  private
+
+  def rooms_params
+    params.require(:rooms).permit(:name)
+  end
+
 end
