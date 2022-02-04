27df8879bacba118d9396b4be976ff687eba60f3
heber <heberlazo@gmail.com>
2022-02-03 23:11:00 -0500

    Added forced clean input

--- app/javascript/extensions/turbolinks.js
+++ app/javascript/extensions/turbolinks.js
@@ -9,3 +9,13 @@ $(document).on("click", "#submit", function (e) {
     $("#message_content").val("");
   }, 10);
 });
+
+// $("#submit").bind("ajax:complete", function (event, xhr, status) {
+//   $("#message_content").val("");
+// });
+
+// $("#submit").submit(function (e) {
+//   e.preventDefault();
+//   this.submit();
+//   $("#message_content").val("");
+// });
